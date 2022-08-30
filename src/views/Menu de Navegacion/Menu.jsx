fb64cb3e52a0c2a13acbf38b825ea29c7760bec4
ramirochangomoreno <rama.exe.13@gmail.com>
2022-08-30 14:02:26 -0300

    e

--- src/views/Menu de Navegacion/Menu.jsx
+++ src/views/Menu de Navegacion/Menu.jsx
@@ -5,10 +5,11 @@ export default function Menu() {
     <div className=" w-2/6 ">
       <ul className="flex items-center justfy-around text-center px-3 gap-3">
         <li className="relative group overflow-hidden">
-          <a
-          className='peer block group-hover:opacity-0 duration-200  '
+        <span className='peer block group-hover:opacity-0 duration-200  ' >¿Fundación Quilla?</span>
+        <a
+        className='absolute top-0 left-0 text-blue-500 translate-x-full group-hover:translate-x-0 duration-300 font-bold'
+          
         href="#queeslafundacion">¿Fundación Quilla?</a>
-        <span className='absolute top-0 left-0 text-blue-500 translate-x-full group-hover:translate-x-0 duration-300 font-bold'>¿Fundación Quilla?</span>
         </li>
         <li>funcion de la fundacion</li>
         <li>programas</li>
